2ff067241b9368269a06faa6a0fe8bbfad2e3e5d
Jose Pedro Lopes Faria <jplfaria@gmail.com>
2016-05-01 23:32:31 -0500

    initial commit of uniprotkb_kw2go ontology translation function

--- lib/ElectronicAnnotationMethods/ElectronicAnnotationMethodsImpl.py
+++ lib/ElectronicAnnotationMethods/ElectronicAnnotationMethodsImpl.py
@@ -487,3 +487,202 @@ class ElectronicAnnotationMethods:
                              'output is not type object as required.')
         # return the results
         return [output]
+
+    def remap_annotations_with_uniprotkb_kw2go(self, ctx, params):
+        # ctx is the context object
+        # return variables are: output
+        #BEGIN remap_annotations_with_uniprotkb_kw2go
+
+        # Print statements to stdout/stderr are captured and available as the method log
+        print('Starting remap_annotations_with_uniprotkb_kw2go method...')
+
+        # Step 1 - Parse/examine the parameters and catch any errors
+        # It is important to check that parameters exist and are defined, and that nice error
+        # messages are returned to the user
+        if 'workspace' not in params:
+            raise ValueError('Parameter workspace is not set in input arguments')
+        workspace_name = params['workspace']
+
+        if 'input_genome' not in params:
+            raise ValueError('Parameter input_genome is not set in input arguments')
+        input_genome = params['input_genome']
+
+        if 'output_genome' not in params:
+            raise ValueError('Parameter output_genome is not set in input arguments')
+        output_genome = params['output_genome']
+
+        ontology_translation = params.get('ontology_translation')
+
+        # Step 2- Download the input data
+        # Most data will be based to your method by its workspace name.  Use the workspace to pull that data
+        # (or in many cases, subsets of that data).  The user token is used to authenticate with the KBase
+        # data stores and other services.  DO NOT PRINT OUT OR OTHERWISE SAVE USER TOKENS
+        token = ctx['token']
+        wsClient = workspaceService(self.workspaceURL, token=token)
+        try:
+            # Note that results from the workspace are returned in a list, and the actual data is saved
+            # in the 'data' key.  So to get the ContigSet data, we get the first element of the list, and
+            # look at the 'data' field.
+            genome = wsClient.get_objects([{'ref': workspace_name+'/'+input_genome}])[0]['data']
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            orig_error = ''.join('    ' + line for line in lines)
+            raise ValueError('Error loading input Genome object from workspace:\n' + orig_error)
+
+        print('Got input genome data.')
+
+        # Load translation object from default or user-specified table
+        translation_ws = workspace_name
+        translation_name = ontology_translation
+        if not translation_name:
+            translation_ws = 'KBaseOntology'
+            translation_name = 'uniprotkb_kw2go'
+        try:
+            translation = wsClient.get_objects([{'ref': translation_ws+'/'+translation_name}])[0]['data']
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            orig_error = ''.join('    ' + line for line in lines)
+            raise ValueError('Error loading OntologyTranslation object from workspace:\n' + orig_error)
+
+        trans = translation['translation']
+        print('Got translation table from {}/{}.'.format(translation_ws, translation_name))
+
+        # Step 3- Actually perform the uniprotkb_kw2go mapping operation
+
+        # Add GO terms to Genome object
+        # print trans
+        n_total_features = 0
+        n_features_mapped = 0
+        for fea in genome['features']:
+            n_total_features += 1
+            fid = fea['id']
+            function = fea.get('function')
+            if not function:
+                continue
+            matches = re.findall('EC[ :][-0-9]+\.[-0-9]+\.[-0-9]+\.[-0-9]+', function)
+            ec_list = self.uniq_seen(matches)
+            print(fid)
+            go_list = []
+            for ec in ec_list:
+                key = ec.replace("EC ", "EC:")
+                equiv_terms = trans.get(key)
+                print key
+                print equiv_terms
+                if equiv_terms:
+                    go = map(lambda x: self.equiv_term_to_string(x), equiv_terms['equiv_terms'])
+                    go_list.extend(go)
+            if len(go_list):
+                n_features_mapped += 1
+                go_func = ' / '.join(sorted(go_list))
+                fea['function'] = go_func
+                print('Mapped {} from "{}"to "{}".'.format(fid, function, go_func))
+
+
+        # Step 4- Save the new Genome back to the Workspace
+        # When objects are saved, it is important to always set the Provenance of that object.  The basic
+        # provenance info is given to you as part of the context object.  You can add additional information
+        # to the provenance as necessary.  Here we keep a pointer to the input data object.
+        provenance = [{}]
+        if 'provenance' in ctx:
+            provenance = ctx['provenance']
+        # add additional info to provenance here, in this case the input data object reference
+        provenance[0]['input_ws_objects']=[workspace_name+'/'+input_genome]
+
+        obj_info_list = None
+        try:
+	        obj_info_list = wsClient.save_objects({
+	                            'workspace':workspace_name,
+	                            'objects': [
+	                                {
+	                                    'type':'KBaseGenomes.Genome',
+	                                    'data':genome,
+	                                    'name':output_genome,
+	                                    'provenance':provenance
+	                                }
+	                            ]
+	                        })
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            orig_error = ''.join('    ' + line for line in lines)
+            raise ValueError('Error saving output Genome object to workspace:\n' + orig_error)
+
+        info = obj_info_list[0]
+        # Workspace Object Info is a tuple defined as-
+        # absolute ref = info[6] + '/' + info[0] + '/' + info[4]
+        # 0 - obj_id objid - integer valued ID of the object
+        # 1 - obj_name name - the name of the data object
+        # 2 - type_string type - the full type of the data object as: [ModuleName].[Type]-v[major_ver].[minor_ver]
+        # 3 - timestamp save_date
+        # 4 - int version - the object version number
+        # 5 - username saved_by
+        # 6 - ws_id wsid - the unique integer valued ID of the workspace containing this object
+        # 7 - ws_name workspace - the workspace name
+        # 8 - string chsum - md5 of the sorted json content
+        # 9 - int size - size of the json content
+        # 10 - usermeta meta - dictionary of string keys/values of user set or auto generated metadata
+
+        print('Saved output Genome:'+pformat(info))
+
+
+        # Step 5- Create the Report for this method, and return the results
+        # Create a Report of the method
+        report = 'New Genome saved to: '+str(info[7]) + '/'+str(info[1])+'/'+str(info[4])+'\n'
+        report += 'Number of total features: '+ str(n_total_features) + '\n'
+        report += 'Number of features mapped to GO terms: '+ str(n_features_mapped) + '\n'
+
+        reportObj = {
+            'objects_created':[{
+                    'ref':str(info[6]) + '/'+str(info[0])+'/'+str(info[4]),
+                    'description':'Genome with annotation remapped using uniprotkb_kw2go'
+                }],
+            'text_message':report
+        }
+
+        # generate a unique name for the Method report
+        reportName = 'uniprotkb_kw2go_report_'+str(hex(uuid.getnode()))
+        report_info_list = None
+        try:
+            report_info_list = wsClient.save_objects({
+                    'id':info[6],
+                    'objects':[
+                        {
+                            'type':'KBaseReport.Report',
+                            'data':reportObj,
+                            'name':reportName,
+                            'meta':{},
+                            'hidden':1, # important!  make sure the report is hidden
+                            'provenance':provenance
+                        }
+                    ]
+                })
+        except:
+            exc_type, exc_value, exc_traceback = sys.exc_info()
+            lines = traceback.format_exception(exc_type, exc_value, exc_traceback)
+            orig_error = ''.join('    ' + line for line in lines)
+            raise ValueError('Error saving report object to workspace:\n' + orig_error)
+
+        report_info = report_info_list[0]
+
+        print('Saved Report: '+pformat(report_info))
+
+        output = {
+                'report_name': reportName,
+                'report_ref': str(report_info[6]) + '/' + str(report_info[0]) + '/' + str(report_info[4]),
+                'output_genome_ref': str(info[6]) + '/'+str(info[0])+'/'+str(info[4]),
+                'n_total_features':n_total_features,
+                'n_features_mapped':n_features_mapped
+            }
+
+        print('Returning: '+pformat(output))
+
+        #END remap_annotations_with_uniprotkb_kw2go
+
+        # At some point might do deeper type checking...
+        if not isinstance(output, object):
+            raise ValueError('Method remap_annotations_with_uniprotkb_kw2go return value ' +
+                             'output is not type object as required.')
+        # return the results
+        return [output]
